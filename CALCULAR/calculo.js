f7633463148cf9a571a819ebdc605582f009e907
Antônio Apolinário Gonzaga Neto <a.apolinario.neto@gmail.com>
2023-01-08 12:17:40 -0300

    Integrando e ajustes
    Nesse commit foi integrado a quantidade de meses com os anos e adicionado uma melhor verificação para os valoresl #4

--- CALCULAR/calculo.js
+++ CALCULAR/calculo.js
@@ -1,19 +1,56 @@
-function VERIFICAR(ano_atual, ano){
+function VERIFICAR_ANO(ano_atual, ano, mes_atual, mes){
 
-    if (ano.value.length == 0){
-        alert("Digite um numero porfavor")
-    } else if (Number(ano.value) > ano_atual) {
-        alert("[ERRO] Digite um ano correto" )
+    if (ano.value.length == 0 || Number(ano.value)<0 || Number(ano.value) > ano_atual){
+        alert("[ERRO] Digite os campos corretos porfavor")
+        location.reload();
+    } else if (mes_atual<Number(mes.value)) {
+        var conta =  ano_atual - Number(ano.value)-1
+        res.innerHTML = `Você tem ${conta} anos`
     } else {
         var conta =  ano_atual - Number(ano.value) 
         res.innerHTML = `Você tem ${conta} anos`
     }
+    
     return conta
 }
+
+function VERIFICAR_MES(mes_atual, mes){
+    if (mes.value.length == 0 || Number(mes.value)<1 || Number(mes.value) >12){
+        alert("[ERRO] Digite os campos corretos porfavor")
+        location.reload();
+    } else if (mes_atual-Number(mes.value) < 0) {
+        qtd_mes = 12-Number(mes.value)+1
+        res.innerHTML += (`, ${qtd_mes} meses` )
+
+    } else {
+        qtd_mes = mes_atual - mes +1
+        res.innerHTML += (`, ${qtd_mes} meses` )
+    }
+
+    
+    return mes_atual
+}
+
+    
+
 function ANO(){
     var res = document.getElementById("res")
     var agora = new Date()
-    VERIFICAR(agora.getFullYear(), ano)
+    res.innerHTML = ""
+    VERIFICAR_ANO(agora.getFullYear(), ano, agora.getMonth(), mes)
+    VERIFICAR_MES(agora.getMonth(), mes)
 
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
